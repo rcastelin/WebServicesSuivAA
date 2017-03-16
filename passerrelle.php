553864cf57fbc6712e5b2aef8e552d6a2d822256
nathan62570 <na.deryckere@gmail.com>
2017-03-16 16:10:54 +0100

    Update passerrelle.php

--- passerrelle.php
+++ passerrelle.php
@@ -2,4 +2,13 @@
 
 class passerrelle {
    
+   function connexion(){
+   $domain="localhost";
+   $bdd = "gsb";
+   $login = "root";
+   $mdp = "mysql";
+   $DB = new PDO('mysql:host='.$domain.';port=3306;dbname='.$bdd.';charset=UTF8;',$login,$mdp)
+   return $DB
+   }
+   
 }
